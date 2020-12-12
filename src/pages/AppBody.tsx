245da27b88bf687e7f7ce90cf558d25509fa428d
Sol Proc <info@kp2r.network>
2020-12-13 03:43:50 +0600

    add kfi

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -6,11 +6,11 @@ export const BodyWrapper = styled.div`
   max-width: 480px;
   width: 100%;
   background: ${({ theme }) => theme.bg1};
-  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
+  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.05), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
-  border-radius: 15px;
+  border-radius: 18px;
   padding: 1rem;
-  border: 2px solid ${({ theme }) => theme.border};
+  border: 3px solid ${({ theme }) => theme.border};
 `
 
 /**
